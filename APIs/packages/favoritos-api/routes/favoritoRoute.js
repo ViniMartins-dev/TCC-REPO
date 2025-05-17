a39667ac483e2f6998dd2a9c804c54affce214bf
vinicius <vinicius_apple22@outlook.com>
2025-05-17 16:28:49 -0300

    feat: adicionando middleware de autenticação nas rotas de favoritos

--- APIs/packages/favoritos-api/routes/favoritoRoute.js
+++ APIs/packages/favoritos-api/routes/favoritoRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const favoritoController = require('../controllers/favoritoController');            // Importa o controlador de favoritos
 const auth = require('../../auth/auth');                                            // Importa o middleware de autenticação
 
-router.post('/', favoritoController.adicionarFavorito);             // Rota para adicionar um animal aos favoritos
-router.delete('/:id_animal/:id_usuario',favoritoController.deletarFavorito);          // Rota para remover um animal dos favoritos
-router.get('/listar/:id',  favoritoController.listarFavorito);   // Rota para listar todos os favoritos de um usuário
+router.post('/', auth.authToken, favoritoController.adicionarFavorito);             // Rota para adicionar um animal aos favoritos
+router.delete('/:id_animal/:id_usuario', auth.authToken, favoritoController.deletarFavorito);          // Rota para remover um animal dos favoritos
+router.get('/listar/:id', auth.authToken, favoritoController.listarFavorito);   // Rota para listar todos os favoritos de um usuário
 
 module.exports = router;
