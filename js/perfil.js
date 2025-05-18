049566bce441cf442e67d57c3a3293583ab84771
Henry-Lima <124622326+Henry-Lima@users.noreply.github.com>
2025-05-18 19:05:12 -0300

    API pagina de perfil
    fiz a API da pagina de perfil puxando os dados nome e telefone para aparecerem ao usuario quando carregar a pagina, com tratativa de erro e distição caso o nome deja de um tutor ou um protetor

--- js/perfil.js
+++ js/perfil.js
@@ -14,3 +14,37 @@ function irParaHome() {
   window.location.href = "./index.html"
 }
 
+const infoPerfil = async () => {
+  try {
+    let perfilUser = await fetch('/usuario', {method: 'GET'})
+    
+    if(!perfilUser.ok) {
+      console.log('Erro ao encontrar dados do Usuário');
+      return;
+    }
+
+    let dados = await perfilUser.json();
+
+    let nomeUs = document.getElementById('nome-user');
+    let tel = document.getElementById('tel-user');
+
+    let nome;
+
+    if(dados.tipo === 'tutor') {
+      nome = dados.nome 
+    } else if(dados.tipo === 'protetor') {
+      nome = dados.nome_fantasia 
+    } else {
+      console.log('Nome não encontrado !!!')
+    }
+    
+    nomeUs.textContent = nome;
+    tel.textContent = dados.telefone;
+    
+  }catch(e) {
+    console.log('Perfil não encontrado');
+  }
+
+}
+
+window.addEventListener('load', infoPerfil);
